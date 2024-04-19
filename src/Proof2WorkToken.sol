79ac9065342e98d9c2036e094b2d55a6a1f3af34
tomtomdu73 <thomas.cosialls@gmail.com>
2024-04-19 18:20:45 -0300

    style: prettier and lint

--- src/Proof2WorkToken.sol
+++ src/Proof2WorkToken.sol
@@ -8,15 +8,19 @@ import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
-
-contract Proof2WorkToken is Initializable, ERC20Upgradeable, ERC20PausableUpgradeable, OwnableUpgradeable, ERC20PermitUpgradeable {
-
+contract Proof2WorkToken is
+    Initializable,
+    ERC20Upgradeable,
+    ERC20PausableUpgradeable,
+    OwnableUpgradeable,
+    ERC20PermitUpgradeable
+{
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
         _disableInitializers();
     }
 
-    function initialize(address initialOwner) initializer public {
+    function initialize(address initialOwner) public initializer {
         __ERC20_init("Proof2Work", "P2W");
         __ERC20Pausable_init();
         __Ownable_init(initialOwner);
@@ -39,18 +43,25 @@ contract Proof2WorkToken is Initializable, ERC20Upgradeable, ERC20PausableUpgrad
 
     function claim() public {
         uint256 amount = 10 * 10 ** decimals();
-        require(balanceOf(owner()) >= amount, "Contract does not have enough tokens to burn");
-        require(balanceOf(msg.sender) < amount, "Proof2Work: you already claimed your tokens");
+        require(
+            balanceOf(owner()) >= amount,
+            "Contract does not have enough tokens to burn"
+        );
+        require(
+            balanceOf(msg.sender) < amount,
+            "Proof2Work: you already claimed your tokens"
+        );
         _burn(owner(), amount);
         _mint(msg.sender, amount);
     }
 
     // The following functions are overrides required by Solidity.
 
-    function _update(address from, address to, uint256 value)
-        internal
-        override(ERC20Upgradeable, ERC20PausableUpgradeable)
-    {
+    function _update(
+        address from,
+        address to,
+        uint256 value
+    ) internal override(ERC20Upgradeable, ERC20PausableUpgradeable) {
         super._update(from, to, value);
     }
 }
